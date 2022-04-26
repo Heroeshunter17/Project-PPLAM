10918e04ad1bd2bf5eb51bf61eff41ffece7e362
Fairuz Afnan <81913620+Heroeshunter17@users.noreply.github.com>
2022-04-26 15:33:45 +0700

    Add files via upload

---
+++ cari.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <title>Document</title>
+</head>
+<body class="bg-light">
+    <div class="container mt-3">
+          <div class="font-monospace text-dark mb-2">
+            <h2>PENCARIAN
+            </h2>
+            </div>
+           <div class="">
+              <form class="d-flex position-relative">
+              <input class="form border-0 col-5 me-3" type="text" placeholder="Search" aria-label="Search" id="Input" onkeyup="searchProduct()">
+              <i class="bi bi-search"></i>
+              </form>
+            </div>
+    </div>
+    <div>
+              
+            </div>
+    <div class="container">
+
+
+
+    </div>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</body>
+</html>
