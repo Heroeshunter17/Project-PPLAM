af8c789f7c0286931721ee1993bfbfcf48e71a92
Rehan Athallah Azhar <72985054+RehanAzhar-Main@users.noreply.github.com>
2022-05-08 14:01:27 +0700

    add :  stock

--- keripikfirda.sql
+++ keripikfirda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2022 at 09:03 AM
+-- Generation Time: May 08, 2022 at 09:00 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -54,6 +54,95 @@ INSERT INTO `bahan_mentah` (`id_bahan`, `nama`, `jenis`, `jumlah`, `banyak_baran
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pemasukan`
+--
+
+CREATE TABLE `pemasukan` (
+  `id_pemasukan` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `keterangan` varchar(200) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total harga` int(11) NOT NULL,
+  `keterangan lain` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemasukan`
+--
+
+INSERT INTO `pemasukan` (`id_pemasukan`, `tanggal`, `keterangan`, `jumlah`, `total harga`, `keterangan lain`) VALUES
+(1, '2022-05-01', 'Kripik Nangka', 10, 50000, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama` char(100) NOT NULL,
+  `jenis_barang` varchar(150) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `deskripsi` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `nama`, `jenis_barang`, `harga`, `deskripsi`) VALUES
+(1, 'Kripik Nangka Firda', 'kripik nangka', 5000, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_barang`
+--
+
+CREATE TABLE `stok_barang` (
+  `id_stok` int(11) NOT NULL,
+  `id_toko` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `stok_dimasukkan` int(11) NOT NULL,
+  `stok_terjual` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stok_barang`
+--
+
+INSERT INTO `stok_barang` (`id_stok`, `id_toko`, `id_produk`, `stok`, `stok_dimasukkan`, `stok_terjual`) VALUES
+(1, 1, 1, 10, 15, 5),
+(2, 2, 1, 10, 15, 5),
+(3, 3, 1, 10, 15, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toko`
+--
+
+CREATE TABLE `toko` (
+  `id_toko` int(11) NOT NULL,
+  `nama toko` char(100) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `deskripsi` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `toko`
+--
+
+INSERT INTO `toko` (`id_toko`, `nama toko`, `alamat`, `deskripsi`) VALUES
+(1, 'Jati Arum', 'Jl. Kapten Suwandak Jogotrunan Lumajang', NULL),
+(2, 'Lumajang Store', 'Jl. Cik Ditiro No. 17 Lumajang', NULL),
+(3, 'Sumber Rasa', 'Jl. Kapten Suwandak no.79', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -82,6 +171,32 @@ ALTER TABLE `bahan_mentah`
   ADD PRIMARY KEY (`id_bahan`);
 
 --
+-- Indexes for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  ADD PRIMARY KEY (`id_pemasukan`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `stok_barang`
+--
+ALTER TABLE `stok_barang`
+  ADD PRIMARY KEY (`id_stok`),
+  ADD KEY `id_toko` (`id_toko`),
+  ADD KEY `id_produk` (`id_produk`);
+
+--
+-- Indexes for table `toko`
+--
+ALTER TABLE `toko`
+  ADD PRIMARY KEY (`id_toko`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -98,10 +213,45 @@ ALTER TABLE `bahan_mentah`
   MODIFY `id_bahan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `stok_barang`
+--
+ALTER TABLE `stok_barang`
+  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `toko`
+--
+ALTER TABLE `toko`
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `stok_barang`
+--
+ALTER TABLE `stok_barang`
+  ADD CONSTRAINT `stok_barang_ibfk_1` FOREIGN KEY (`id_toko`) REFERENCES `toko` (`id_toko`),
+  ADD CONSTRAINT `stok_barang_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
